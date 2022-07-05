418f8c3f649cc3a19ff04312940731945effc2c9
vudinhson2001 <you@example.com>
2022-07-05 10:35:14 +0700

    clone

--- src/main/java/com/example/airbnb/service/ICategoryService.java
+++
@@ -1,7 +0,0 @@
-package com.example.airbnb.service;
-
-
-import com.example.airbnb.model.Category;
-
-public interface ICategoryService extends IService<Category>{
-}
--- src/main/java/com/example/airbnb/service/IProductService.java
+++
@@ -1,9 +0,0 @@
-package com.example.airbnb.service;
-
-import com.example.airbnb.model.Product;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
-
-public interface IProductService extends IService<Product>{
-    Page<Product> findAllByNameContaining(String name, Pageable pageable);
-}
